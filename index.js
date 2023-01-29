0b42dbcd8d4f8a6b6fd0f45493a3d308bf905d0b
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2023-01-29 20:48:41 -0300

    progress

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const { parse } = require('csv-parse');
+const { execSync, exec } = require('child_process');
+const spawn = require('cross-spawn');
 
-const csvData = {};
+let csvArray = [];
+let csvData = {};
 let firstFlag = true;
 let header;
 fs.createReadStream('csv input estudo empírico.csv')
@@ -15,8 +18,69 @@ fs.createReadStream('csv input estudo empírico.csv')
       csvrow.forEach((csv) => {
         csvData[header[idx++]] = csv.includes('$$') ? csv.split('$$') : csv;
       });
+      csvArray.push(csvData);
+      csvData = {};
     }
   })
   .on('end', function () {
-    console.log(csvData);
+    executeStudy(csvArray);
   });
+
+const executeStudy = (csvArray) => {
+  console.log(csvArray);
+
+  const execute = (csv, n) => {
+    const aplicacao = n === 1 ? '1_aplicacao' : '2_aplicacao';
+    const aplicacaoUrl = n === 1 ? '1_aplicacao_url' : '2_aplicacao_url';
+
+    if (fs.existsSync(`workstation/${csv[aplicacao]}`)) {
+      execSync(`cd workstation && rm -R ${csv[aplicacao]}`);
+    }
+    execSync(
+      `cd workstation && git clone https://github.com/gui-testing-study/${csv[aplicacao]}.git`
+    );
+    [
+      `${n}_falta_1`,
+      `${n}_falta_2`,
+      `${n}_falta_3`,
+      `${n}_falta_4`,
+      `${n}_falta_5`,
+    ].forEach((falta) => {
+      const filePath = `workstation/${csv[aplicacao]}/${csv[falta][0]}`;
+      let file = fs.readFileSync(filePath).toString();
+      fs.writeFileSync(filePath, file.replace(csv[falta][1], csv[falta][2]));
+    });
+    execSync(
+      'PID=`ps -eaf | lsof -t -i:8080 | grep -v grep`; if [[ "" !=  "$PID" ]]; then echo "killing $PID"; kill -9 $PID; fi;'
+    );
+    if (csv[aplicacao].includes('spring')) {
+      execSync(`cd workstation/${csv[aplicacao]} && ./mvnw spring-boot:run`);
+    } else {
+      exec(
+        `cd workstation/${csv[aplicacao]} && python3 -m http.server 8080`,
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+    execSync('sleep 10');
+
+    let status = 0;
+    const options = ['generate-test:dev'];
+    const execCytestion = () => {
+      const execution = spawn.sync('yarn', options, {
+        stdio: 'inherit',
+        cwd: '/home/thiagomoura/workspace/mestrado/gui-testing-exercise/cytestion/',
+      });
+      if (execution.status !== 0) status = execution.status;
+    };
+
+    execCytestion();
+    execSync(`cd workstation && rm -R ${csv[aplicacao]}`);
+  };
+
+  csvArray.forEach((csv) => {
+    execute(csv, 1);
+    execute(csv, 2);
+  });
+};
---
+++ init-repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd workstation
+git clone https://github.com/gui-testing-study/spring-petclinic.git
+git clone https://github.com/gui-testing-study/spring-shop-n3xt.git
+git clone https://github.com/gui-testing-study/website-bistro-restaurant.git
+git clone https://github.com/gui-testing-study/website-learn-educational.git
+git clone https://github.com/gui-testing-study/website-school-educational.git

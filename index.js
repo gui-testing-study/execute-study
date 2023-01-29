da0e69b5c4483b83ff8ad9743a95da69bf2378d5
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2023-01-29 09:54:57 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const csvData = {};
+let firstFlag = true;
+let header;
+fs.createReadStream('csv input estudo empírico.csv')
+  .pipe(parse({ delimiter: ',' }))
+  .on('data', function (csvrow) {
+    if (firstFlag) {
+      firstFlag = false;
+      header = csvrow;
+    } else {
+      let idx = 0;
+      csvrow.forEach((csv) => {
+        csvData[header[idx++]] = csv.includes('$$') ? csv.split('$$') : csv;
+      });
+    }
+  })
+  .on('end', function () {
+    console.log(csvData);
+  });
